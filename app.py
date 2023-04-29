688156e7e5e98b3abcea0a0ff53b72178e732086
Anthony Jones <anthony15jones@gmail.com>
2023-04-29 13:17:52 -0500

    Parcial.1

--- app.py
+++ app.py
@@ -1,68 +1,52 @@
-from sqlite3 import apilevel
-from modelo import ProductoCRUD
-from unittest import result
-
-productoCRUD = ProductoCRUD()
-
-while True:
-    print('....INVENTARIO....')
-    print('1. Agregar')
-    print('2. visualizar')
-    print('3. Actualizar')
-    print('4. Eliminar')
-    print('5. Buscar')
-    print('0. Salir')
-    opcion = int(input('Opcion: '))
-
-    if opcion == 2:
-        productos = productoCRUD.mostrar()
-        if len(productos) == 0:
-            print('No hay productos')
-        else:
-            for i in productos:
-                print('-' * 30)
-                print(f'Id: {i[0]}\nNombre: {i[1]}\nPrecio: {i[2]}')
-
-    elif opcion == 1:
-        resultado = productoCRUD.agregar(
-            nombre=input('Ingresar producto: '),
-            precio=float(input('Ingresar precio: '))
-        )
-
-        if resultado:
-            print('Producto agregado!')
-
-    elif opcion == 3:
-        id = input('Ingrese el id del producto: ')
-        producto = productoCRUD.existe(id)
-        if producto:
-            nombre = input(f'Ingrese el nuevo nombre({producto[1]}): ')
-            precio = input(f'Igrese el nuevo precio({producto[2]}): ')
-
-            if len(nombre) == 0:
-                nombre = producto[1]
-            if len(precio) == 0:
-                precio = producto[2]
-
-            resultado = productoCRUD.actualizar(id, nombre, precio)
-            if resultado:
-                print('Producto actualizado')
-        else:
-            print('El id del producto no existe')
-    elif opcion == 4:
-        id = input('Ingresar id del producto: ')
-        resultado = productoCRUD.eliminar(id)
-        if resultado:
-            print('Producto eliminado!')
-    elif opcion == 5:
-        id = input('Ingresar id del producto: ')
-        producto = productoCRUD.existe(id)
-        if producto:
-            print(f'Id: {producto[0]}\nNombre: {producto[1]}\nPrecio: {producto[2]}')
-        else:
-            print('El id del producto no existe')
-    elif opcion == 0:
-        break
-
+if opcion == 2:
+    productos = productoCRUD.mostrar()
+    if len(productos) == 0:
+        print('No hay productos')
     else:
-        print('Opcion incorrecta')
+        for i in productos:
+            print('-' * 30)
+            print(f'Id: {i[0]}\nNombre: {i[1]}\nPrecio: {i[2]}')
+
+elif opcion == 1:
+    resultado = productoCRUD.agregar(
+        nombre=input('Ingrese el nombre del producto: '),
+        precio=float(input('Ingrese el precio del producto: '))
+    )
+
+    if resultado:
+        print('Producto agregado exitosamente!')
+
+elif opcion == 3:
+    id = input('Ingrese el ID del producto: ')
+    producto = productoCRUD.existe(id)
+    if producto:
+        nombre = input(f'Ingrese el nuevo nombre ({producto[1]}): ')
+        precio = input(f'Ingrese el nuevo precio ({producto[2]}): ')
+
+        if len(nombre) == 0:
+            nombre = producto[1]
+        if len(precio) == 0:
+            precio = producto[2]
+
+        resultado = productoCRUD.actualizar(id, nombre, precio)
+        if resultado:
+            print('Producto actualizado exitosamente!')
+    else:
+        print('El ID del producto no existe')
+elif opcion == 4:
+    id = input('Ingrese el ID del producto: ')
+    resultado = productoCRUD.eliminar(id)
+    if resultado:
+        print('Producto eliminado exitosamente!')
+elif opcion == 5:
+    id = input('Ingrese el ID del producto: ')
+    producto = productoCRUD.existe(id)
+    if producto:
+        print(f'Id: {producto[0]}\nNombre: {producto[1]}\nPrecio: {producto[2]}')
+    else:
+        print('El ID del producto no existe')
+elif opcion == 0:
+    break
+
+else:
+    print('Opción incorrecta')
--- parcial.py
+++ parcial.py
@@ -1,60 +1,52 @@
-import sqlite3
-
-
-class ProductoCRUD(object):
+def __init__(self) -> None:
     """
-    Clase para realizar las operaciones CRUD sobre la tabla producto
-    La tabla palabra esta compuesta por id,nombre,precio
+    Automatically connect to the database.
     """
+    self.connection = sqlite3.connect("inventory.db")
+    self.cursor = self.connection.cursor()
 
-    def __init__(self) -> None:
-        """
-        Conexion autmatica a la base de datos
-        """
-        self.conexion = sqlite3.connect("inventario.db")
-        self.cursor = self.conexion.cursor()
+def show(self):
+    try:
+        self.cursor.execute('SELECT * FROM product')
+        return self.cursor.fetchall()
+    except Exception as error:
+        print(error)
+        return []
 
-    def mostrar(self):
-        try:
-            self.cursor.execute('select * from producto')
-            return self.cursor.fetchall()
-        except Exception as error:
-            print(error)
-            return []
+def add(self, name, price):
+    try:
+        self.cursor.execute(f"INSERT INTO product(name, price) VALUES('{name}',{price})")
+        self.connection.commit()
+        return True
+    except Exception as error:
+        print(error)
+        return False
 
-    def agregar(self, nombre, precio):
-        try:
-            self.cursor.execute(f"insert into producto(nombre,precio) values('{nombre}',{precio})")
-            self.conexion.commit()
+def update(self, id, name, price):
+    try:
+        self.cursor.execute(f"UPDATE product SET name='{name}', price={price} WHERE id={id}")
+        self.connection.commit()
+        return True
+    except Exception as error:
+        print(error)
+        return False
+
+def delete(self, id):
+    try:
+        if (self.exists(id)):
+            self.cursor.execute(f"DELETE FROM product WHERE id={id}")
+            self.connection.commit()
             return True
-        except Exception as error:
-            print(error)
-            return False
+        print('Error: The product ID does not exist!')
+        return False
+    except Exception as error:
+        print(error)
+        return False
 
-    def actualizar(self, id, nombre, precio):
-        try:
-            self.cursor.execute(f"update producto set nombre='{nombre}',precio={precio} where id={id}")
-            self.conexion.commit()
-            return True
-        except Exception as error:
-            print(error)
-            return False
+def exists(self, id):
+    try:
+        self.cursor.execute(f"SELECT * FROM product WHERE id={id}")
+        return self.cursor.fetchone()
+    except:
+        return None
 
-    def eliminar(self, id):
-        try:
-            if (self.existe(id)):
-                self.cursor.execute(f"delete from producto where id={id}")
-                self.conexion.commit()
-                return True
-            print('Error: El id del producto no existe!')
-            return False
-        except Exception as error:
-            print(error)
-            return False
-
-    def existe(self, id):
-        try:
-            self.cursor.execute(f"select * from producto where id={id}")
-            return self.cursor.fetchone()
-        except:
-            return None
